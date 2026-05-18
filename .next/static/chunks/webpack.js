fe7e087b04b488d21cea1eaa72893ce84d06dbdc
TanishSen <tanishsen.2520@gmail.com>
2026-05-18 15:19:35 +0530

    Add hot-update files for Webpack with eval-source-map devtool
    - Created new hot-update files: webpack.7edeeb16834ac7ba.hot-update.js and webpack.a2bb3b4a531b36fe.hot-update.js.
    - Both files include a warning about the use of "eval-source-map" devtool, which is not suitable for production.
    - Updated the webpack runtime modules to include new hash values for hot module replacement.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "32cf5b9482618407"; }
+/******/ 		__webpack_require__.h = function() { return "4a638e0d8240cd6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
